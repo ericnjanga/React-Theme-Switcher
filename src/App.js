b31a508a4b40ebc436025eb3bb4c045deb46d5c2
Eric Njanga <njanga.webdev@gmail.com>
2018-07-10 18:35:49 -0400

    setting up basic components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ThemeSelector from './components/ThemeSelector/ThemeSelector.js';
+import Navbar from './components/Navbar/Navbar.js';
+import Hero from './components/Hero/Hero.js';
+import Article from './components/Article/Article.js';
+import Footer from './components/Footer/Footer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ThemeSelector />
+        <Navbar />
+        <main role="main">
+          <Hero />
+          <div className="container">
+            <div className="row">
+              <Article />
+              <Article />
+              <Article />
+            </div>
+            <hr />
+          </div>
+        </main>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  
+  return (
+    <footer class="container">
+      <p>&copy; Company 2018</p>
+    </footer>
+  );
+
+};
+
+export default Footer;
